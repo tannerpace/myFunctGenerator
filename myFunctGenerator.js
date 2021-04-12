40ae0acc1c9a74d11f2bc5b5465e7f3e9da86e17
TannerB <tannerpace@gmail.com>
2021-04-12 17:17:19 -0400

    Add files via upload
    I uploaded myFunctGenerator.js

---
+++ myFunctGenerator.js
@@ -0,0 +1,25 @@
+
+
+   
+a = Number(prompt("enter a number"))
+ b = Number(prompt("enter a number"))
+ c = prompt("enter an operator")
+
+if (c == "+"){
+    answer = (a + b)
+}
+else if (c == "*"){
+    answer = (a * b)
+}
+ 
+else if (c == "-"){
+    answer = (a - b)
+}
+
+else if(c == "/"){
+    answer = (a/b)}
+
+
+
+
+prompt(answer)
